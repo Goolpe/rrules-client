fae9d30e3045bf5cb7d4ff726c63986ef08b1db9
Artur Khabirov <gooolpe@gmail.com>
2018-08-26 15:23:06 +0500

    arrowUp

---
+++ src/ArrowUp.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Link, Events, animateScroll as scroll} from 'react-scroll';
+
+class ArrowUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+	    arrowUp: false
+    }
+	this.onScroll = this.onScroll.bind(this);
+  }	
+
+    onScroll(){
+    	if (window.pageYOffset > 100){
+    		this.setState({arrowUp: true})
+    	}
+    	else{
+    		this.setState({arrowUp: false})
+    	}
+	}
+   	componentDidMount(){
+   	 	window.addEventListener('scroll', this.onScroll);
+	}
+
+	scrollToTop() {
+	    scroll.scrollToTop();
+	  }
+  	render() {
+	    return (
+	    	<div>
+	    	{this.state.arrowUp && <div id="arrowUp" onClick={this.scrollToTop}><i className="fas fa-angle-up fa-3x"></i></div>}
+	    	</div>
+	    );
+	}
+}
+
+export default ArrowUp;
