e6ed2033fd99f5feeac73f3fb22e477beaed5293
Artur Khabirov <gooolpe@gmail.com>
2018-08-28 16:00:45 +0500

    view in users and master lists

--- src/components/UsersPage/MastersPage.js
+++ src/components/UsersPage/MastersPage.js
@@ -44,7 +44,7 @@ class MastersPage extends Component {
 	 					<Link to={`/@${master.username}`} className="text-dark ">	
 		 					<div className="row p-3 text-left align-items-start userCard mb-4 shadow-sm">
 		 						<div className="col-12 col-md-4">
-		 							<p className="m-0">Мастер: {master.username}</p>
+		 							<p className="m-0">{master.username}</p>
 		 						</div>
 		 						<div className="col-12 col-md-4">
 		 							<p className="m-0">Рейтинг: {master.rating}</p>
