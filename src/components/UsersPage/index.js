e28b0acb413eb7b37b9e743720d567cf8e38ff00
Artur Khabirov <gooolpe@gmail.com>
2018-08-28 15:50:02 +0500

    view in users and master lists

--- src/components/UsersPage/index.js
+++ src/components/UsersPage/index.js
@@ -63,7 +63,7 @@ class UsersPage extends Component {
 					<div className="d-flex justify-content-end align-items-center pb-2">
 						<p className="m-0">Вид:</p>
 						<button className="btn bg-transparent ml-2" onClick={()=>this.setState({viewList: false})}><i className={this.state.viewList ? "text-dark fas fa-th-large fa-2x" : "text-info fas fa-th-large fa-2x"}></i></button>
-						<button className="btn bg-transparent" onClick={()=>this.setState({viewList: true})}><i  className={this.state.viewList ? "text-info fas fa-list fa-2x" : "text-dark fas fa-list fa-2x"}></i></button>
+						<button className="btn bg-transparent" onClick={()=>this.setState({viewList: true})}><i  className={this.state.viewList ? "text-info fas fa-th-list fa-2x" : "text-dark fas fa-th-list fa-2x"}></i></button>
 				    </div>
 					<div className="row">
 					 	{this.state.viewList ? usersList : usersView  }
