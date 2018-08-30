8cc17a5466a2c1887f6892f22cf9af1a0de86ac1
Artur Khabirov <gooolpe@gmail.com>
2018-08-30 20:10:03 +0500

    youtube

--- src/components/HomePage/videos.js
+++ src/components/HomePage/videos.js
@@ -7,16 +7,16 @@ class Videos extends Component {
 				<p className="text-white font-weight-bold">ПОСЛЕДНИЕ ВИДЕО</p>
 	    		<div className="row text-white">
 	    			<div className="col-12 col-md-6 col-lg-3 mt-2">
-	    			<iframe width="100%" title="video1" height="300" src="https://www.youtube.com/embed/?listType=playlist&list=PL8eGlCwLLio5vV_yunVLJ-BCyNlPf6MWV" frameBorder="0" allowFullScreen></iframe>
+	    			<iframe width="100%" title="video1" height="300" src="https://www.youtube.com/embed/videoseries?index=0&list=UU-Hexiu7bmDvDlE6tA7LECw" frameBorder="0" allowFullScreen></iframe>
 	    			</div>
 	    			<div className="col-12 col-md-6 col-lg-3 mt-2">
-	    			<iframe width="100%" title="video2" height="300" src="https://www.youtube.com/embed/?listType=playlist&list=PL8eGlCwLLio5bVBfJWzTbusNP3l2I8Vy-" frameBorder="0" allowFullScreen></iframe>
+	    			<iframe width="100%" title="video2" height="300" src="https://www.youtube.com/embed/videoseries?index=1&list=UU-Hexiu7bmDvDlE6tA7LECw" frameBorder="0" allowFullScreen></iframe>
+	    			</div>
+					<div className="col-12 col-md-6 col-lg-3 mt-2">
+	    			<iframe width="100%" title="video3" height="300" src="https://www.youtube.com/embed/videoseries?index=2&list=UU-Hexiu7bmDvDlE6tA7LECw" frameBorder="0" allowFullScreen></iframe>
 					</div>
 					<div className="col-12 col-md-6 col-lg-3 mt-2">
-	    			<iframe width="100%" title="video3" height="300" src="https://www.youtube.com/embed/?listType=playlist&list=PL8eGlCwLLio4jk7NudmwjG71iTFEIGlwA" frameBorder="0" allowFullScreen></iframe>
-					</div>
-					<div className="col-12 col-md-6 col-lg-3 mt-2">
-	    			<iframe width="100%" title="video4" height="300" src="https://www.youtube.com/embed/?listType=playlist&list=PL8eGlCwLLio70j93h8k4tR_ATj4A7liAh" frameBorder="0" allowFullScreen></iframe>
+	    			<iframe width="100%" title="video4" height="300" src="https://www.youtube.com/embed/videoseries?index=3&list=UU-Hexiu7bmDvDlE6tA7LECw" frameBorder="0" allowFullScreen></iframe>
 					</div>
 	    		</div>
 	    	</div>
