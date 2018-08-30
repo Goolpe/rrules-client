a194679ba59e88c70a955fe6255da828997dbe0f
Artur Khabirov <gooolpe@gmail.com>
2018-08-30 20:27:14 +0500

    youtube

--- src/components/HomePage/videos.js
+++ src/components/HomePage/videos.js
@@ -7,16 +7,16 @@ class Videos extends Component {
 				<p className="text-white font-weight-bold">ПОСЛЕДНИЕ ВИДЕО</p>
 	    		<div className="row text-white">
 	    			<div className="col-12 col-md-6 col-lg-3 mt-2">
-	    			<iframe width="100%" title="video1" height="300" src="http://www.youtube.com/embed/videoseries?list=UU-Hexiu7bmDvDlE6tA7LECw" frameBorder="0" allowFullScreen></iframe>
+	    			<iframe width="100%" title="video1" height="300" src="https://www.youtube.com/embed/videoseries?list=UU-Hexiu7bmDvDlE6tA7LECw" frameBorder="0" allowFullScreen></iframe>
 	    			</div>
 	    			<div className="col-12 col-md-6 col-lg-3 mt-2">
-	    			<iframe width="100%" title="video2" height="300" src="http://www.youtube.com/embed/videoseries?list=UU-Hexiu7bmDvDlE6tA7LECw&index=1" frameBorder="0" allowFullScreen></iframe>
+	    			<iframe width="100%" title="video2" height="300" src="https://www.youtube.com/embed/videoseries?list=UU-Hexiu7bmDvDlE6tA7LECw&index=1" frameBorder="0" allowFullScreen></iframe>
 	    			</div>
 					<div className="col-12 col-md-6 col-lg-3 mt-2">
-	    			<iframe width="100%" title="video3" height="300" src="http://www.youtube.com/embed/videoseries?list=UU-Hexiu7bmDvDlE6tA7LECw&index=2" frameBorder="0" allowFullScreen></iframe>
+	    			<iframe width="100%" title="video3" height="300" src="https://www.youtube.com/embed/videoseries?list=UU-Hexiu7bmDvDlE6tA7LECw&index=2" frameBorder="0" allowFullScreen></iframe>
 					</div>
 					<div className="col-12 col-md-6 col-lg-3 mt-2">
-	    			<iframe width="100%" title="video4" height="300" src="http://www.youtube.com/embed/videoseries?list=UU-Hexiu7bmDvDlE6tA7LECw&index=3" frameBorder="0" allowFullScreen></iframe>
+	    			<iframe width="100%" title="video4" height="300" src="https://www.youtube.com/embed/videoseries?list=UU-Hexiu7bmDvDlE6tA7LECw&index=3" frameBorder="0" allowFullScreen></iframe>
 					</div>
 	    		</div>
 	    	</div>
