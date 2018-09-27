d0a377c956419cb9e0dad5d7031f3205ffc7a1e9
Artur Khabirov <gooolpe@gmail.com>
2018-09-27 16:04:04 +0500

    msgs

--- src/components/MessagesPage/index.js
+++ src/components/MessagesPage/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchPlayers } from '../actions/playerActions';
 import { changeGameData, fetchGames } from '../actions/gameActions';
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaPlus, FaMinus } from "react-icons/fa";
 
 class Msgs extends Component {
 	constructor(props) {
@@ -57,27 +57,27 @@ class Msgs extends Component {
 	 	var messagesItems = this.props.games.filter(game => game.name === this.props.auth.user.player).map(game =>
 	 		game.gamersInsideId.filter(gamer => gamer.show === true)
 	 		.map((msg,index) => 
-	 			<div className="shadow bg_card text_card p-3 mb-3" key={index}>
+	 			<div className="shadow bg_card text_card pb-2 pl-2 pr-2 mb-3" key={index}>
 		 			<div className="row align-items-center">
-						<div className="col-12 col-md-4">
+						<div className="col-12 col-md-2 mt-2">
 							{this.props.players.filter(player=> msg.user === player._id).map((player, index)=>
-								<React.Fragment key={index}>Отправитель: <Link target="_blank" className="mr-3" to={`/@${player.name}`}>{player.name}</Link></React.Fragment>
+								<React.Fragment key={index}><Link target="_blank" className="mr-3" to={`/@${player.name}`}>{player.name}</Link></React.Fragment>
 							)}
 						</div>
-						<div className="col-12 col-md-3">
-							Игра: <Link target="_blank" to={`/game/${game._id}`}>{game.nameGame}</Link>
+						<div className="col-12 col-md-6 mt-2">
+							<Link target="_blank" to={`/game/${game._id}`}>{game.nameGame}</Link>
 						</div>
-						<div className="col-12 col-md-4">
+						<div className="col-12 col-md-3 mt-2">
 							{!msg.accept && !msg.decline ?
 								<React.Fragment>
-									<button className="btn btn-info mr-3" onClick={()=>{this.handleAccept(game._id, msg.user)}}>Подтвердить</button>
-									<button className="btn btn-danger mr-3" onClick={()=>{this.handleDecline(game._id, msg.user)}}>Отклонить</button>
+									<button className="btn btn-success mr-3" onClick={()=>{this.handleAccept(game._id, msg.user)}}><FaPlus /></button>
+									<button className="btn btn-danger mr-3" onClick={()=>{this.handleDecline(game._id, msg.user)}}><FaMinus /></button>
 								</React.Fragment>
 							:
-								msg.accept ? "Вы добавили игрока" : "Отклонен"
+								msg.accept ? "Добавлен" : "Отклонен"
 							}
 						</div>
-						<div className="col-12 col-md-1">
+						<div className="col-12 col-md-1 mt-2">
 							<button className="btn bg-transparent text_card userpage__facog" onClick={()=>{this.handleDelete(game._id, msg.user, msg.accept, msg.decline)}}><FaTimes size="1.5em"/></button>
 						</div>
 					</div>
